72512dd479388c28a117d37f209e7b523ec91048
Jigar Lodaya <jigar.lodaya12@gmail.com>
2021-04-27 18:58:38 +0530

    Updated script

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+  console.log("ready!");
 
-    var details = [];
-    // var url = `https://covid-19-hospital-data.el.r.appspot.com/area/Delhi`;
-    var url = `https://covidmapindia.org/area/Delhi`;
-  
+  var details = [];
+  // var url = `https://covid-19-hospital-data.el.r.appspot.com/area/Delhi`;
+  var url = `https://covidmapindia.org/area/Delhi`;
 
-
-    $.get( url, function( data ) {
-        var values = data.values;
-        var head;
+  $.get( url, function( data ) {
+      var values;
+      var head;
+      if ( typeof data['values'] === 'object' ) {
+        console.log("i am here");
+        values = data.values;
         values.forEach((element, index) => {
           if ( index == 0 ) {
             head=element;
@@ -21,41 +22,34 @@ $( document ).ready(function() {
             details.push(d);
           }
         });
-        // mak?eTable(details);
-        // let rows = [];
-        // values.forEach((element, index) => {
-        //   let row = "";
-        //   element.forEach(ele => {
-        //     row += "<td>" + ele + "</td>";
-        //   });
-        //   rows += "<tr>" + row + "</tr>";
-
-        // });
-        // console.log(rows);
-        // $("#tablecont").html(rows);
-        $.fn.dataTable.ext.errMode = 'none';
-        $('#example').DataTable( {
-          data: details,
-          columns: [
-            { data: "Location" },
-            { data: 'Name' },
-            { data: 'Type' },
-            { data: "Available beds"},
-            { data: "Beds last updated"},
-            { data: "Available ICU beds"},
-            { data: "ICU beds last updated"},
-            { data: "Available ventilators"},
-            { data: "Ventilators last updated"},
-            { data: "ICU without ventilator"},
-            { data: "ICU without ventilator last updated"},
-            { data: "Oxygen availability"},
-            { data: "Verification source"},
-            { data: "Phone"},
-            { data: "Comments"},
-        ]    
-      } );
-        // alert( "Load was performed." );
-      });
-
+      } else {
+         details = data;
+      }
       
+      $.fn.dataTable.ext.errMode = 'none';
+      $('#example').DataTable( {
+        data: details,
+        columns: [
+          { data: "Location" },
+          { data: 'Name' },
+          { data: 'Type' },
+          { data: "Available beds"},
+          { data: "Beds last updated"},
+          { data: "Available ICU beds"},
+          { data: "ICU beds last updated"},
+          { data: "Available ventilators"},
+          { data: "Ventilators last updated"},
+          { data: "ICU without ventilator"},
+          { data: "ICU without ventilator last updated"},
+          { data: "Oxygen availability"},
+          { data: "Verification source"},
+          { data: "Phone"},
+          { data: "Comments"},
+      ],
+      // "order": [[ 6, "dsc" ]]
+    } );
+      // alert( "Load was performed." );
+    });
+
+  
 });
